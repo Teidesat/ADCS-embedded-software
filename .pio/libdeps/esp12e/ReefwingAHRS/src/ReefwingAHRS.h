bfebd46caad0e45a20850c626e0f02fe04a962d0
Aram-Perez-Dios <aramperezdios@gmail.com>
2024-04-29 23:55:34 +0100

    updated serial output information format

--- .pio/libdeps/esp12e/ReefwingAHRS/src/ReefwingAHRS.h
+++ .pio/libdeps/esp12e/ReefwingAHRS/src/ReefwingAHRS.h
@@ -166,14 +166,11 @@ class LSM9DS1 {
     void loadGyroBias(float gxB, float gyB, float gzB);
     void loadMagBias(float mxB, float myB, float mzB);
 
-    void readAccelData(int16_t* destination);
-    void readGyroData(int16_t* destination);
-    void readMagData(int16_t* destination);
-
     Quaternion getQuaternion();
     EulerAngles update();
     SelfTestResults selfTest();
     BiasOffsets getBiasOffsets();
+
     SensorData filterFormat();
     SensorData rawData();
     uint32_t sampleRate = 100; // Hz
@@ -181,9 +178,9 @@ class LSM9DS1 {
   private:
     uint8_t whoAmIGyro();
     uint8_t whoAmIMag();
-    //void readAccelData(int16_t* destination);
-    //void readGyroData(int16_t* destination);
-    //void readMagData(int16_t* destination);
+    void readAccelData(int16_t* destination);
+    void readGyroData(int16_t* destination);
+    void readMagData(int16_t* destination);
     void writeByte(uint8_t address, uint8_t subAddress, uint8_t data);
     uint8_t readByte(uint8_t address, uint8_t subAddress);
     void readBytes(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t* dest);
